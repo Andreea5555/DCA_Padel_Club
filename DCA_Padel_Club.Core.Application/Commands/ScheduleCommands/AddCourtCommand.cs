12cf7c067c8edfdae7a0c8cd120a8a9560c15316
Andreea5555 <151520266+Andreea5555@users.noreply.github.com>
2026-04-06 22:41:12 +0200

    adding more commands and handlers

---
+++ DCA_Padel_Club.Core.Application/Commands/ScheduleCommands/AddCourtCommand.cs
@@ -0,0 +1,25 @@
+using DCA_Padel_Club.Core.Domain.Aggregates.Schedule;
+using DCA_Padel_Club.Core.Domain.Aggregates.Schedules;
+using DCA_Padel_Club.Core.Tools.OperationResult;
+
+namespace DCA_Padel_Club.Core.Application.Commands.ScheduleCommands;
+
+public class AddCourtCommand
+{
+    public ScheduleId ScheduleId { get; }
+    public CourtId CourtId { get; }
+
+    private AddCourtCommand(ScheduleId scheduleId, CourtId courtId)
+    {
+        ScheduleId = scheduleId;
+        CourtId = courtId;
+    }
+
+    public static Result<AddCourtCommand> Create(string scheduleId, string courtId)
+    {
+        return Result<AddCourtCommand>.Success(
+            new AddCourtCommand(
+                new ScheduleId(Guid.Parse(scheduleId)),
+                new CourtId(courtId)));
+    }
+}
---
+++ DCA_Padel_Club.Core.Application/Features/ScheduleFeatures/AddCourtHandler.cs
@@ -0,0 +1,43 @@
+using DCA_Padel_Club.Core.Application.Commands.ICommandHandler;
+using DCA_Padel_Club.Core.Application.Commands.ScheduleCommands;
+using DCA_Padel_Club.Core.Domain.Aggregates.Schedules;
+using DCA_Padel_Club.Core.Domain.Common.Contracts;
+using DCA_Padel_Club.Core.Tools.OperationResult;
+
+namespace DCA_Padel_Club.Core.Application.Features;
+
+public class AddCourtHandler: ICommandHandler<AddCourtCommand>
+{
+    private readonly IScheduleRepository Repository;
+    private readonly IUnitOfWork Uow;
+    private readonly ICurrentDate CurrentDate;
+
+    public AddCourtHandler(IScheduleRepository repository,
+        IUnitOfWork unitOfWork, ICurrentDate currentDate)
+    {
+        Repository = repository;
+        Uow = unitOfWork;
+        CurrentDate = currentDate;
+    }
+
+    public async Task<Result<None>> HandleAsync(AddCourtCommand command)
+    {
+        Schedule? schedule = await Repository.GetAsync(command.ScheduleId);
+
+        if (schedule is null)
+        {
+            return Result<None>.Failure(new List<OperationError>
+            {
+                OperationError.Create("Schedule.NotFound", "No schedule found")
+            });
+        }
+
+        Result<None> result = schedule.AddCourt(command.CourtId, CurrentDate);
+        if (result.IsSuccess)
+        {
+            await Uow.SaveChangesAsync();
+        }
+        
+        return result;
+    }
+}
