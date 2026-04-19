f23d331d9bda6970335bb1a231bae3a2d259c697
xvvcs <maciek.m2004@wp.pl>
2026-04-19 21:08:23 +0200

    add configurations and converters to efc db context

--- src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence/EfcDbContext.cs
+++ src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence/EfcDbContext.cs
@@ -1,12 +1,25 @@
+using DCA_Padel_Club.Core.Domain.Aggregates.Players;
+using DCA_Padel_Club.Core.Domain.Aggregates.Schedules;
+using DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence.ValueConverters;
 using Microsoft.EntityFrameworkCore;
 
 namespace DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence;
 
-public class EfcDbContext(DbContextOptions options): DbContext(options)
+public class EfcDbContext(DbContextOptions<EfcDbContext> options) : DbContext(options)
 {
+    public DbSet<Player> Players => Set<Player>();
+    public DbSet<Schedule> Schedules => Set<Schedule>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(EfcDbContext).Assembly);
     }
-    
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<ViaId>().HaveConversion<ViaIdConverter>();
+        configurationBuilder.Properties<ScheduleId>().HaveConversion<ScheduleIdConverter>();
+        configurationBuilder.Properties<BookingId>().HaveConversion<BookingIdConverter>();
+        configurationBuilder.Properties<CourtId>().HaveConversion<CourtIdConverter>();
+    }
 }
