02de1a5974ac6b614e01f65a246a07166be8705f
xvvcs <maciek.m2004@wp.pl>
2026-05-10 17:27:10 +0200

    fix Player handlers to use scaffolded context

--- src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcQueries/Handlers/Player/PlayerOverviewHandler.cs
+++ src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcQueries/Handlers/Player/PlayerOverviewHandler.cs
@@ -1,32 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using DCA_Padel_Club.Core.QueryContracts.Abstractions;
 using DCA_Padel_Club.Core.QueryContracts.Queries;
-using DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence;
 
 namespace DCA_Padel_Club.Infrastructure.EfcQueries.Handlers.Player;
 
-public class PlayerOverviewHandler : IQueryHandler<PlayerOverview.Query, PlayerOverview.Answer>
+public class PlayerOverviewHandler(ViapadelClubContext context)
+    : IQueryHandler<PlayerOverview.Query, PlayerOverview.Answer>
 {
-    private readonly EfcDbContext _context;
-
-    public PlayerOverviewHandler(EfcDbContext context)
-    {
-        _context = context;
-    }
-
     public async Task<PlayerOverview.Answer> HandleAsync(PlayerOverview.Query query)
     {
-        var players = await _context.Players
+        var players = await context.Players
             .Select(p => new PlayerOverview.PlayerInfo(
-                p.Id.Value,
-                p.FirstName.Value,
-                p.LastName.Value,
-                p.Email.Value,
-                p.Blacklisted
+                p.Id,
+                p.FirstName,
+                p.LastName,
+                p.Email,
+                p.Blacklisted == 1
             ))
             .ToListAsync();
 
         return new PlayerOverview.Answer(players);
     }
 }
-
