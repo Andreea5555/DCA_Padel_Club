3bf359449b90757ed0cd2e1703bdaa4a361d6a3c
xvvcs <maciek.m2004@wp.pl>
2026-05-10 16:43:35 +0200

    add recommended QuearyDbExtensions

--- src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcQueries/ViapadelClubContext.cs
+++ src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcQueries/ViapadelClubContext.cs
@@ -25,8 +25,12 @@ public partial class ViapadelClubContext : DbContext
     public virtual DbSet<ScheduleCourt> ScheduleCourts { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=/Users/maciej/VIA/6thSem/DCA/DCA_assignment/DCA_Padel_Club/src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence/VIAPadelClub.db");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=/Users/maciej/VIA/6thSem/DCA/DCA_assignment/DCA_Padel_Club/src/Infrastructure/DCA_Padel_Club.Infrastructure.EfcDomainModelPersistence/VIAPadelClub.db");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
