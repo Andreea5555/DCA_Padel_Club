9c4e199b843a190979c6ab06eaf2a415a287e9c6
xvvcs <maciek.m2004@wp.pl>
2026-05-10 17:25:36 +0200

    fix fake current time to use proper CurrentTime service

--- DCA_Padel_Club.Core.Domain/Common/Contracts/ICurrentTime.cs
+++ DCA_Padel_Club.Core.Domain/Common/Contracts/ICurrentTime.cs
@@ -3,4 +3,5 @@ namespace DCA_Padel_Club.Core.Domain.Common.Contracts;
 public interface ICurrentTime
 {
     TimeOnly Now { get; }
+    DateTime CurrentTime();
 }
--- UnitTests/Fakes/FakeCurrentTime.cs
+++ UnitTests/Fakes/FakeCurrentTime.cs
@@ -2,9 +2,25 @@ using DCA_Padel_Club.Core.Domain.Common.Contracts;
 
 namespace UnitTests.Fakes;
 
-internal class FakeCurrentTime(TimeOnly now) : ICurrentTime
+internal class FakeCurrentTime : ICurrentTime
 {
-    public TimeOnly Now => now;
+    private readonly TimeOnly _timeOnly;
+    private readonly DateTime _dateTime;
 
-    public static FakeCurrentTime RealNow() => new FakeCurrentTime(TimeOnly.FromDateTime(DateTime.Now));
+    public FakeCurrentTime(TimeOnly now)
+    {
+        _timeOnly = now;
+        _dateTime = DateTime.Today.Add(now.ToTimeSpan());
+    }
+
+    public FakeCurrentTime(DateTime now)
+    {
+        _dateTime = now;
+        _timeOnly = TimeOnly.FromDateTime(now);
+    }
+
+    public TimeOnly Now => _timeOnly;
+    public DateTime CurrentTime() => _dateTime;
+
+    public static FakeCurrentTime RealNow() => new(DateTime.Now);
 }
